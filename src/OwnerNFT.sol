ae2aab1b0b9f25a6322dcd60ffbed39ed1fedf64
uzairnadeem201 <uzairnadeem201@gmail.com>
2025-03-11 23:26:40 +0500

    OwnerNFT Test File done

--- src/OwnerNFT.sol
+++ src/OwnerNFT.sol
@@ -4,6 +4,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OwnerNFT is ERC721, Ownable {
+    error NotOwner(string message);
+    error TokenDoesNotExist(uint256 tokenId);
     mapping(uint256 => string) private _tokenURIs;
     uint256 private _tokenIdCounter;
 
@@ -16,11 +18,16 @@ contract OwnerNFT is ERC721, Ownable {
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        require(_ownerOf(tokenId) != address(0), "Token does not exist"); // Check via _ownerOf
+        if (_ownerOf(tokenId) == address(0)) {
+            revert TokenDoesNotExist(tokenId);
+        }
         return _tokenURIs[tokenId];
     }
 
-    function mintNFT(address to, string memory uri) public onlyOwner {
+    function mintNFT(address to, string memory uri) public {
+        if (msg.sender != owner()) {
+            revert NotOwner("Unautorized User is requesting to Mint");
+        }
         _tokenIdCounter++;
         uint256 tokenId = _tokenIdCounter;
         _mint(to, tokenId);
--- test/OwnerNft.t.sol
+++ test/OwnerNft.t.sol
@@ -6,8 +6,44 @@ import {OwnerNFT} from "../src/OwnerNFT.sol";
 contract OwnerNFTTest is Test {
     OwnerNFT private nftContract;
     address private owner;
-    address private recipient;
-    function setUp public(){
-      
+    address private unauthorizedUser;
+    string private constant TOKEN_URI = "ipfs://example-token-uri";
+
+    function setUp() public {
+        owner = vm.addr(1);
+        unauthorizedUser = vm.addr(2);
+        vm.prank(owner);
+        nftContract = new OwnerNFT();
+    }
+
+    function testOnlyOwnerCanMint() public {
+        vm.prank(owner);
+        nftContract.mintNFT(owner, TOKEN_URI);
+        assertEq(nftContract.ownerOf(1), owner);
+    }
+
+    function testUnauthorizedUserCannotMint() public {
+        vm.prank(unauthorizedUser);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OwnerNFT.NotOwner.selector,
+                "Unautorized User is requesting to Mint"
+            )
+        );
+
+        nftContract.mintNFT(unauthorizedUser, TOKEN_URI);
+    }
+
+    function testSetTokenUriandRetrievingIt() public {
+        vm.prank(owner);
+        nftContract.mintNFT(owner, TOKEN_URI);
+        assertEq(nftContract.tokenURI(1), TOKEN_URI);
+    }
+
+    function testTokenURINonExistentTokenShouldRevert() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(OwnerNFT.TokenDoesNotExist.selector, 9999)
+        );
+        nftContract.tokenURI(9999);
     }
 }
